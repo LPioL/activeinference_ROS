4f05e8c217688645b50b9512b841d56f61671aac
LPioL <leo.pio.lopez@gmail.com>
2016-11-10 16:57:37 +0100

    Update SevDofthreeDv1simpleROS_spm_gx_robot_adem_reach.m

--- SevDofthreeDv1simpleROS_spm_gx_robot_adem_reach.m
+++ SevDofthreeDv1simpleROS_spm_gx_robot_adem_reach.m
@@ -14,31 +14,7 @@ function [g] = SevDofthreeDv1simpleROS_spm_gx_robot_adem_reach(x,v,a,P)
 % P    - parameters
 % a    - action
 % P    - parameters
-%__________________________________________________________________________
-% Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
- 
-% Karl Friston
-% $Id: spm_gx_adem_reach.m 3893 2010-05-17 18:28:52Z karl $
 
-% stretch (angular) and visual (positional) information (target & arm)
-%==========================================================================
-
-% x = full(x);
-% pub = rospublisher('/r_arm_controller/command',rostype.trajectory_msgs_JointTrajectory);
-% message = rosmessage('trajectory_msgs/JointTrajectory');
-% message.JointNames = {'r_shoulder_pan_joint';'r_shoulder_lift_joint';'r_upper_arm_roll_joint';
-%     'r_elbow_flex_joint';'r_forearm_roll_joint';'r_wrist_flex_joint';'r_wrist_roll_joint'} ;
-% point=rosmessage('trajectory_msgs/JointTrajectoryPoint');
-% message.Points=point;
-% pos = x(1:7);
-% velo = x(8:14);
-% %acc = [0.5;0.5;0;0;0;0;0];
-% message.Points.Positions = pos;
-% message.Points.Velocities = velo;
-% message.Points.Accelerations = a;
-% send(pub,message)
-% pause (10)
-% clear pub message
 
 
 g = SevDofthreeDv1simpleROS_spm_gx_robot_dem_reach(x,v,P);
